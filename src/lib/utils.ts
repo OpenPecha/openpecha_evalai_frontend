6853325c526744afe65c88079a4805090041ad80
tenkus47 <tenkus47@gmail.com>
2025-09-15 16:17:03 +0530

    feat: add time formatting utility for improved response time display in UserVoteLeaderboard component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function timeFormatSeconds(seconds: number) {
+  if (seconds < 60) {
+    return `${seconds}s`;
+  } else if (seconds < 3600) {
+    return `${Math.floor(seconds / 60)}m`;
+  } else {
+    return `${Math.floor(seconds / 3600)}h`;
+  }
+}
